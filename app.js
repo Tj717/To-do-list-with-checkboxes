f27b8d61e65f4afef5a35bae3166fe72e291ac55
taijing <jingtai@uw.edu>
2022-07-04 20:56:26 -0700

    final v

---
+++ app.js
@@ -0,0 +1,10 @@
+// window.onload=function(){
+//     var resetForm = function(){
+//         $('#form')[0].reset();
+//     }
+    
+//     const btn = document.getElementById('reset');
+    
+//     btn.addEventListener(resetForm);
+//   }
+
